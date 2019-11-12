86514989b84f34b3d454b4af6fe8bb171ccb1598
leogtzr <leogutierrezramirez@gmail.com>
2019-11-11 17:17:06 -0700

    add base code for strategy pattern

---
+++ src/java-design-patterns/src/main/java/com/dp/strategy/StrategyApp.java
@@ -0,0 +1,15 @@
+package com.dp.strategy;
+
+import com.dp.strategy.domain.TextFormatter;
+
+public class StrategyApp {
+
+    private static void showText(final TextFormatter formatter, final String input) {
+        System.out.println(formatter.format(input));
+    }
+
+    public static void main(final String[] args) {
+        // TODO: Fix this ...
+        // showText();
+    }
+}
---
+++ src/java-design-patterns/src/main/java/com/dp/strategy/domain/TextFormatter.java
@@ -0,0 +1,5 @@
+package com.dp.strategy.domain;
+
+public interface TextFormatter {
+    String format(String input);
+}
